8d549d2c85931607568969522d91abab792f7ebe
Trung Anh <trantrunganh1505@gmail.com>
2022-10-25 11:51:45 +0700

    tta update

--- pages/pre/course/Course.jsx
+++ pages/pre/course/Course.jsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getNews } from "../../../store/redux/NewsReducer/news.action";
 import { removeAccents } from "../../../utils/Function";
 import styles from "./Course.module.scss";
+import ReactPaginate from "react-paginate";
 
 export default function Course(props) {
+  const itemsPerPage = 6;
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
   const router = useRouter();
   const dispatch = useDispatch();
   const { news } = useSelector((state) => state.NewsReducer);
@@ -14,6 +20,51 @@ export default function Course(props) {
   useEffect(() => {
     dispatch(getNews());
   }, [dispatch]);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(news.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(news.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, news]);
+
+  function Items({ currentItems }) {
+    return (
+      <div className="d-flex flex-wrap">
+        {currentItems?.map((item, index) => (
+          <div className="col-12 col-sm-6 d-flex flex-wrap pe-5" key={index}>
+            <div className="col-6">
+              <Image
+                loader={({ src }) =>
+                  `https://api.fostech.vn${src}?access_token=${process.env.ACCESS_TOKEN}`
+                }
+                alt="khoa_hoc"
+                src={item.picture}
+                width={430}
+                height={250}
+              />
+            </div>
+            <div className="col-6 px-3">
+              <p
+                className={styles.course_title}
+                onClick={() =>
+                  router.push(`/${removeAccents(item?._id || "")}`)
+                }
+              >
+                {item.title}
+              </p>
+              <p className={styles.course_content}>{item.mieu_ta_ngan}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % news.length;
+
+    setItemOffset(newOffset);
+  };
 
   return (
     <div className={styles.course}>
@@ -23,7 +74,7 @@ export default function Course(props) {
       </div>
 
       <section className="d-flex flex-wrap">
-        <div className={styles.bdRight + " " + "col-12 col-sm-8 pe-0 pe-sm-4"}>
+        <div className={"col-12 col-sm-8 pe-0 pe-sm-4"}>
           <div className="d-flex flex-wrap">
             <div className="col-12">
               <Image
@@ -32,8 +83,8 @@ export default function Course(props) {
                 }
                 alt="khoa_hoc"
                 src={news[0]?.picture}
-                width={900}
-                height={405}
+                width={1200}
+                height={535}
               />
             </div>
 
@@ -64,7 +115,7 @@ export default function Course(props) {
                     alt="khoa_hoc"
                     src={item.picture}
                     width={430}
-                    height={210}
+                    height={250}
                   />
                 </div>
 
@@ -84,7 +135,9 @@ export default function Course(props) {
         </div>
       </section>
 
-      <section className="d-flex flex-wrap pb-3 pb-sm-5 ">
+      <div className="line"></div>
+
+      {/* <section className="d-flex flex-wrap pb-3 pb-sm-5 ">
         <div className={styles.bdRight + " " + "col-12 col-sm-8 pe-0 pe-sm-4"}>
           {news.slice(3, 6).map((item, index) => {
             return (
@@ -154,14 +207,169 @@ export default function Course(props) {
             })}
           </div>
         </div>
+      </section> */}
+
+      <section className="mb-4">
+        {/* {news.slice(0, 6).map((item, index) => {
+          return (
+            <div className="col-12 col-sm-6 d-flex flex-wrap pe-5">
+              <div className="col-6">
+                <Image
+                  loader={({ src }) =>
+                    `https://api.fostech.vn${src}?access_token=${process.env.ACCESS_TOKEN}`
+                  }
+                  alt="khoa_hoc"
+                  src={item.picture}
+                  width={430}
+                  height={250}
+                />
+              </div>
+              <div className="col-6 px-3">
+                <p
+                  className={styles.course_title}
+                  onClick={() =>
+                    router.push(`/${removeAccents(item?._id || "")}`)
+                  }
+                >
+                  {item.title}
+                </p>
+                <p className={styles.course_content}>{item.mieu_ta_ngan}</p>
+              </div>
+            </div>
+          );
+        })} */}
+
+        <div
+          className="position-relative h-100"
+          style={{ paddingBottom: "70px" }}
+        >
+          <Items currentItems={currentItems} />
+
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            previousLabel=" < "
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={2}
+            pageCount={pageCount}
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            pageLinkClassName="page-num"
+            previousLinkClassName="page-num"
+            nextLinkClassName="page-num"
+            activeLinkClassName="active"
+          />
+        </div>
+      </section>
+
+      <section className="d-flex flex-row mb-4">
+        <div
+          className="col-12 d-flex flex-wrap justify-content-around px-2 px-lg-4 pt-5 pt-sm-3 py-3 py-lg-4 "
+          style={{ backgroundColor: "#606060" }}
+        >
+          <div className="heading_white px-3 px-lg-5">
+            <p className="text">phương thức</p>
+            <p className="col decor"></p>
+          </div>
+
+          {news.slice(0, 3).map((item, index) => (
+            <div className="col-12 col-sm-4 px-3 mb-3 mb-sm-0" key={index}>
+              <Image
+                loader={({ src }) =>
+                  `https://api.fostech.vn${src}?access_token=${process.env.ACCESS_TOKEN}`
+                }
+                alt="co_che_von"
+                src={item.picture}
+                width={440}
+                height={250}
+              />
+              <p
+                className={styles.course_title_white}
+                onClick={() =>
+                  router.push(`/${removeAccents(item?._id || "")}`)
+                }
+              >
+                {item.title}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="d-flex flex-row mb-4">
+        <div
+          className="col-12 d-flex flex-wrap justify-content-around px-2 px-lg-4 pt-5 pt-sm-3 py-3 py-lg-4 "
+          style={{ backgroundColor: "#606060" }}
+        >
+          <div className="heading_white px-3 px-lg-5">
+            <p className="text">HỒ SƠ GỌI VỐN</p>
+            <p className="col decor"></p>
+          </div>
+
+          {news.slice(0, 3).map((item, index) => (
+            <div className="col-12 col-sm-4 px-3 mb-3 mb-sm-0" key={index}>
+              <Image
+                loader={({ src }) =>
+                  `https://api.fostech.vn${src}?access_token=${process.env.ACCESS_TOKEN}`
+                }
+                alt="co_che_von"
+                src={item.picture}
+                width={440}
+                height={250}
+              />
+              <p
+                className={styles.course_title_white}
+                onClick={() =>
+                  router.push(`/${removeAccents(item?._id || "")}`)
+                }
+              >
+                {item.title}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="d-flex flex-row mb-4">
+        <div
+          className="col-12 d-flex flex-wrap justify-content-around px-2 px-lg-4 pt-5 pt-sm-3 py-3 py-lg-4 "
+          style={{ backgroundColor: "#606060" }}
+        >
+          <div className="heading_white px-3 px-lg-5">
+            <p className="text">HỒ SƠ THƯƠNG HIỆU</p>
+            <p className="col decor"></p>
+          </div>
+
+          {news.slice(0, 3).map((item, index) => (
+            <div className="col-12 col-sm-4 px-3 mb-3 mb-sm-0" key={index}>
+              <Image
+                loader={({ src }) =>
+                  `https://api.fostech.vn${src}?access_token=${process.env.ACCESS_TOKEN}`
+                }
+                alt="co_che_von"
+                src={item.picture}
+                width={440}
+                height={250}
+              />
+              <p
+                className={styles.course_title_white}
+                onClick={() =>
+                  router.push(`/${removeAccents(item?._id || "")}`)
+                }
+              >
+                {item.title}
+              </p>
+            </div>
+          ))}
+        </div>
       </section>
 
       <section>
         <Image
           alt="khoa_hoc"
           src="/images/course/banner.png"
-          width={1380}
-          height={280}
+          width={1850}
+          height={420}
         />
       </section>
     </div>
