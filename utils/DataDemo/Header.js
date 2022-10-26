378790ca4dd5728fb19608e129079b8437d8b727
theanh <tranluutheanh17601@gmail.com>
2022-10-26 15:11:46 +0700

    tlta fix active menu

--- utils/DataDemo/Header.js
+++ utils/DataDemo/Header.js
@@ -2,74 +2,82 @@ export const HeaderData = [
   {
     id: 1,
     title: "TRANG CHỦ",
-    listTag: [""],
+    // listTag: [""],
     link: "/",
+    tag:"/"
   },
   {
     id: 2,
     title: "KIẾN THỨC IPO",
     listTag: [
       { tag: "Cơ Chế Vốn", link: "/knowledge/mechanism" },
-      { tag: "Quỹ Đầu Tư", link: "/knowledge/mechanism" },
+      { tag: "Quỹ Đầu Tư", link: "#" },
       { tag: "Thuật Ngữ", link: "/knowledge/dictionary" },
     ],
     link: "#",
+    tag:"knowledge"
   },
   {
     id: 3,
     title: "PRE IPO",
     listTag: [
       { tag: "Khoá học", link: "/pre/course" },
-      { tag: "Phương thức", link: "/pre/course" },
-      { tag: "Hồ sơ gọi vốn", link: "/pre/course" },
-      { tag: "Hồ sơ thương hiệu", link: "/pre/course" },
+      { tag: "Phương thức", link: "#" },
+      { tag: "Hồ sơ gọi vốn", link: "#" },
+      { tag: "Hồ sơ thương hiệu", link: "#" },
     ],
     link: "#",
+    tag:"pre"
   },
   {
     id: 4,
     title: "RUNNING TO IPO",
     listTag: [
       { tag: "Gallery", link: "/running/gallery" },
-      { tag: "Theo dòng sự kiện", link: "/running/gallery" },
+      { tag: "Theo dòng sự kiện", link: "#" },
     ],
     link: "#",
+    tag:"running"
   },
   {
     id: 5,
     title: "CHUYÊN GIA IPO",
     listTag: [
       { tag: "Phỏng vấn chuyên gia", link: "/expert" },
-      { tag: "Chân dung nhân vật", link: "/expert" },
+      { tag: "Chân dung nhân vật", link: "#" },
     ],
     link: "#",
+    tag:"expert"
   },
   {
     id: 6,
     title: "TIN TỨC IPO",
     listTag: [
       { tag: "Thế giới", link: "/news" },
-      { tag: "Trong nước", link: "/news" },
-      { tag: "Sự kiện", link: "/news" },
-      { tag: "Phỏng vấn chuyên gia", link: "/news" },
-      { tag: "Chân dung nhân vật", link: "/news" },
+      { tag: "Trong nước", link: "#" },
+      { tag: "Sự kiện", link: "#" },
+      { tag: "Phỏng vấn chuyên gia", link: "#" },
+      { tag: "Chân dung nhân vật", link: "#" },
     ],
     link: "#",
+    tag:"news"
   },
   {
     id: 7,
     title: "THƯ VIỆN IPO",
     listTag: [
       { tag: "Case Study Doanh Nghiệp", link: "/library/case-study" },
-      { tag: "Case Study Aura Capital", link: "/library/case-study" },
+      { tag: "Case Study Aura Capital", link: "#" },
       { tag: "Tài liệu IPO", link: "#" },
     ],
     link: "#",
+    tag:"library"
   },
   {
     id: 8,
     title: "LIÊN HỆ",
-    listTag: [""],
+    // listTag: [""],
     link: "/contact",
+    tag:"contact"
   },
 ];
