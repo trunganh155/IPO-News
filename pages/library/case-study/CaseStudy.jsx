de848be13a5631aa78931709630f8c4ebcc7bb9c
Trung Anh <trantrunganh1505@gmail.com>
2022-10-28 09:36:49 +0700

    fix bug

--- pages/library/case-study/CaseStudy.jsx
+++ pages/library/case-study/CaseStudy.jsx
@@ -41,9 +41,7 @@ export default function CaseStudy(props) {
         </div> */}
 
         <div
-          className={
-            styles.modalFilter + " " + "col-3 pe-4 d-flex flex-column"
-          }
+          className={styles.modalFilter + " " + "col-3 pe-4 d-flex flex-column"}
         >
           {/* {Array(2)
             .fill()
