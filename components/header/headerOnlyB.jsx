8637be5f0b112c9bcbc2f4324ea46198a47a9612
theanh <tranluutheanh17601@gmail.com>
2022-10-31 08:57:15 +0700

    tlta update

--- components/header/headerOnlyB.jsx
+++ components/header/headerOnlyB.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { BiSearch } from "react-icons/bi";
+import { IoCloseOutline } from "react-icons/io5";
 import styles from "./headerOnly.module.scss";
 function HeaderOnlyB(props) {
 	const router = useRouter();
+	const [isShowSearch, setIsShowSearch] = useState(true);
 	return (
 		<Navbar
 			expand="lg"
@@ -26,20 +28,74 @@ function HeaderOnlyB(props) {
 					>
 						<div
 							className={
-								styles.header_mid +
+								[styles.header_mid, styles.padding_b].join(
+									" "
+								) +
 								" " +
-								"px-2 d-flex justify-content-center flex-column-reverse align-items-center flex-md-row align-self-center"
+								"px-2 d-flex justify-content-center align-items-center flex-md-row align-self-center"
 							}
 						>
+							<div className="d-md-none d-flex col-6 col-md-5 col-xl-4 my-3 my-md-0 align-self-center">
+								<div className={styles.search}>
+									{isShowSearch ? (
+										<button
+											onClick={() => {
+												setIsShowSearch(!isShowSearch);
+											}}
+										>
+											<BiSearch
+												size={35}
+												color={"#B5B5B5"}
+											/>
+										</button>
+									) : (
+										<>
+											<Form
+												style={{
+													padding: "0 17px 0 20px",
+												}}
+												className="d-flex justify-content-center align-items-center w-100"
+											>
+												<Form.Control
+													type="search"
+													placeholder="Tìm kiếm"
+													className={
+														styles.navSearch +
+														" " +
+														"w-90"
+													}
+													aria-label="Tìm kiếm"
+												/>
+												<div
+													className="d-flex align-items-center"
+													style={{
+														cursor: "pointer",
+													}}
+													onClick={() => {
+														setIsShowSearch(
+															!isShowSearch
+														);
+													}}
+												>
+													<IoCloseOutline
+														size={35}
+														color={"#B5B5B5"}
+													/>
+												</div>
+											</Form>
+										</>
+									)}
+								</div>
+							</div>
 							<div
 								className={
 									styles.search +
 									" " +
-									"d-flex col-8 col-md-5 col-xl-4 my-3 my-md-0 align-self-center"
+									"d-md-flex d-none col-7 col-md-5 col-xl-4 my-3 my-md-0 align-self-center"
 								}
 							>
 								<Form
-									style={{ padding: "0 17px 0 79px" }}
+									style={{ padding: "0 17px 0 20px" }}
 									className="d-flex justify-content-center align-items-center w-100"
 								>
 									<Form.Control
@@ -50,11 +106,15 @@ function HeaderOnlyB(props) {
 										}
 										aria-label="Tìm kiếm"
 									/>
-									<BiSearch size={35} color={"#B5B5B5"}/>
+									<BiSearch size={35} color={"#B5B5B5"} />
 								</Form>
 							</div>
-							<div className={"col-md-2 col-xl-4"}></div>
-							<div className="col-10 col-md-5 col-xl-4 d-flex justify-content-between">
+							<div
+								className={
+									"col-1 col-md-2 col-xl-4"
+								}
+							></div>
+							<div className="col-5 col-xl-4 d-flex justify-content-between">
 								<Image
 									src="/images/logo_white.png"
 									alt="logo"
--- components/header/headerOnlyW.jsx
+++ components/header/headerOnlyW.jsx
@@ -5,6 +5,7 @@ import { Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { BiSearch } from "react-icons/bi";
+import { IoCloseOutline } from "react-icons/io5";
 import styles from "./headerOnly.module.scss";
 function HeaderOnlyW(props) {
 	const router = useRouter();
@@ -44,14 +45,14 @@ function HeaderOnlyW(props) {
 							</div>
 							<div
 								className={
-									"d-none d-md-block col-md-2 col-xl-4"
+									"col-1 col-md-2 col-xl-4"
 								}
 							></div>
 							<div
 								className={
 									styles.search +
 									" " +
-									"d-flex col-7 col-md-5 col-xl-4 my-3 my-md-2 align-self-center"
+									"d-none d-md-flex col-7 col-md-5 col-xl-4 my-3 my-md-2 align-self-center"
 								}
 							>
 								<Form
@@ -69,38 +70,59 @@ function HeaderOnlyW(props) {
 									<BiSearch size={35} color={"#B5B5B5"} />
 								</Form>
 							</div>
-							{/* <div
-								className={
-									styles.search +
-									" " +
-									"d-md-none col-7 col-md-5 col-xl-4 my-3 my-md-2 align-self-center justify-content-end d-flex"
-								}
-							>
-								{isShowSearch ? (
-									<button
-										onClick={() => {
-											setIsShowSearch(!isShowSearch);
-										}}
-									>
-										<BiSearch size={35} color={"#B5B5B5"} />
-									</button>
-								) : (
-									<Form
-										style={{ padding: "0 17px 0 20px" }}
-										className="d-flex justify-content-center align-items-center w-100"
-									>
-										<Form.Control
-											type="search"
-											placeholder="Tìm kiếm"
-											className={
-												styles.navSearch + " " + "w-90"
-											}
-											aria-label="Tìm kiếm"
-										/>
-										<BiSearch size={35} color={"#B5B5B5"} />
-									</Form>
-								)}
-							</div> */}
+							<div className="d-md-none col-6 col-md-5 col-xl-4 my-3 my-md-2 align-self-center justify-content-end d-flex">
+								<div className={styles.search}>
+									{isShowSearch ? (
+										<button
+											onClick={() => {
+												setIsShowSearch(!isShowSearch);
+											}}
+										>
+											<BiSearch
+												size={35}
+												color={"#B5B5B5"}
+											/>
+										</button>
+									) : (
+										<>
+											<Form
+												style={{
+													padding: "0 17px 0 20px",
+												}}
+												className="d-flex justify-content-center align-items-center w-100"
+											>
+												<Form.Control
+													type="search"
+													placeholder="Tìm kiếm"
+													className={
+														styles.navSearch +
+														" " +
+														"w-90"
+													}
+													aria-label="Tìm kiếm"
+												/>
+												{/* <BiSearch size={35} color={"#B5B5B5"} /> */}
+												<div
+													className="d-flex align-items-center"
+													style={{
+														cursor: "pointer",
+													}}
+													onClick={() => {
+														setIsShowSearch(
+															!isShowSearch
+														);
+													}}
+												>
+													<IoCloseOutline
+														size={35}
+														color={"#B5B5B5"}
+													/>
+												</div>
+											</Form>
+										</>
+									)}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
