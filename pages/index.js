c7e16af789d48136af0454dc854350849f96c772
theanh <tranluutheanh17601@gmail.com>
2022-11-02 17:13:00 +0700

    tlta update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import HomePage from "./Home/HomePage";
+import HomeUser from "./Home/HomeUser";
+import Cookies from "js-cookie";
 
 export default function Home() {
-  return <HomePage />;
+	const [isLogin, setIsLogin] = useState(false);
+
+	useEffect(() => {
+		Cookies.get("token") && setIsLogin(true);
+	}, []);
+	return <>{isLogin ? <HomeUser /> : <HomePage />}</>;
 }
