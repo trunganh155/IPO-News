60b68aede3411c34503b436d7f269cd4c53b8a83
Trung Anh <trantrunganh1505@gmail.com>
2022-11-08 09:46:27 +0700

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,13 +18,6 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
   jwt: {
