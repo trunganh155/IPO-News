81ecdfc53747cbb9a8bdca1ba510c015e89dc19c
Trung Anh <trantrunganh1505@gmail.com>
2022-12-05 10:11:43 +0700

    update

---
+++ pages/running/gallery/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../../../components/layout/mainLayout";
+import Gallery from "./Gallery";
+
+function Index(props) {
+  return (
+    <MainLayout>
+      <Gallery />
+    </MainLayout>
+  );
+}
+
+export default Index;
